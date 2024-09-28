4575e2f9b4839e1d96184b0d974e99f7b3a61fd5
Jirayu-ninl <Jirayu.ninl@gmail.com>
2024-09-29 05:00:23 +0700

    [feat] storage: update s3 upload route

---
+++ src/app/api/upload/image/route.ts
@@ -0,0 +1,84 @@
+import { NextResponse } from 'next/server'
+import { headers } from 'next/headers'
+import { uuidv7 } from 'uuidv7'
+import { s3 } from '@nexel/nextjs/libs/storage'
+import { getSession } from '@backend/auth/aurora'
+import { convertFromReadableStream } from '@nexel/nextjs/utils/data'
+import { optimize } from '@nexel/nextjs/utils/image'
+import { setResponse } from '@nexel/nextjs/utils/server/response.status'
+
+export const PUT = async (req: Request) => {
+  const session = await getSession()
+
+  if (!session || !session.user) {
+    return setResponse.unauthorized()
+  }
+
+  try {
+    const headersList = headers()
+    const _fileSize = parseInt(headersList.get('content-size') ?? '0')
+    // const _fileType = headersList.get('content-Type')
+
+    const { searchParams } = new URL(req.url)
+    const _dir = searchParams.get('dir')
+    const _bucketSuffix = searchParams.get('bucketSuffix')
+
+    // if (!name) throw new Error('File name not provided')
+    const imageKey = uuidv7()
+
+    const data = await convertFromReadableStream.toBuffer(
+      req.body as ReadableStream,
+    )
+    if (_fileSize !== data.length) throw new Error('Lost data while uploading')
+
+    const name = `${_dir}.${imageKey}.jpg`
+
+    const bucketName = _bucketSuffix
+      ? process.env.S3_UPLOAD_BUCKET + '.' + _bucketSuffix
+      : process.env.S3_UPLOAD_BUCKET
+
+    const isAvatar =
+      _bucketSuffix === 'users' && _dir?.split('.')[0] === 'avatar'
+    const optimizeConfig = isAvatar
+      ? {
+          maxWidth: 256,
+          maxHeight: 256,
+          quality: 80,
+        }
+      : {
+          maxWidth: 2048,
+          maxHeight: 2048,
+          quality: 50,
+        }
+
+    const optimizeImageBuffer = await optimize.optimizeAndConvertToJpg(
+      data,
+      optimizeConfig,
+    )
+
+    const upload = await s3.putObject({
+      bucket: bucketName!,
+      key: name,
+      file: {
+        body: optimizeImageBuffer,
+        contentType: 'image/jpeg',
+        metadata: {
+          'Image-Key': `${_dir}.${imageKey}`,
+          'User-Id': session.user.id,
+        },
+      },
+    })
+    return NextResponse.json({ success: true, upload, id: imageKey, name })
+  } catch (e) {
+    if (
+      typeof e === 'object' &&
+      e &&
+      'message' in e &&
+      typeof e.message === 'string'
+    ) {
+      throw new Error(e.message)
+    } else {
+      throw new Error('Error while uploading')
+    }
+  }
+}

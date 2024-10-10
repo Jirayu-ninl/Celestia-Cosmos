5cb118ced40fe643585e23b0d003bc36cb3c8e9c
Jirayu-ninl <Jirayu.ninl@gmail.com>
2024-10-10 10:15:51 +0700

    [test] k6: add metrics endpoint to health check

--- tests/k6/health.js
+++ tests/k6/health.js
@@ -1,6 +1,6 @@
 import http from 'k6/http'
 import { check } from 'k6'
-import { SERVICE_URLS, TEST_USER } from './constants'
+import { SERVICE_URLS } from './constants'
 
 export const options = {
   vus: 1, // VUs to run concurrently.
@@ -11,10 +11,25 @@ export const options = {
 }
 
 export default function () {
+  const metrics = {}
   Object.entries(SERVICE_URLS).forEach((service_name, service_url) => {
-    const res = http.get(service_url)
-    check(res, {
+    const res = http.get(service_url + '/health')
+    const isHealthy = check(res, {
       [`${service_name} is healthy`]: (r) => r.status === 200,
     })
+    metrics[service_name] = { isHealthy }
   })
+
+  const payload = JSON.stringify({
+    projectId: 'celestia',
+    key: 'health_check',
+    metrics: metrics,
+  })
+
+  const headers = { 'Content-Type': 'application/json' }
+  const res = http.post(process.env.METRICS_URL, payload, {
+    headers,
+  })
+  console.log(`Metrics POST response status: ${res.status}`)
+  console.log(`Metrics POST response body: ${res.body}`)
 }

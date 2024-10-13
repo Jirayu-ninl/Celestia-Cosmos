5547bb0c05e5bea965591b1f7f4c8bd648734978
Jirayu-ninl <Jirayu.ninl@gmail.com>
2024-10-14 01:33:53 +0700

    [feat] auth/user: add user dashboard (placeholder)

---
+++ src/app/(app)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Client } from './page.client'
+const Page = () => {
+  // TODO: User Dashboard
+  return (
+    <>
+      <div className='pt-8'>
+        <Client />
+      </div>
+    </>
+  )
+}
+
+export default Page

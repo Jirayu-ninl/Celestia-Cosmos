721d5bfe0246c1f2246a56af1ae4fadbedab6aef
Jirayu-ninl <Jirayu.ninl@gmail.com>
2024-10-16 21:29:26 +0700

    [feat] search: add search function (modal)

--- src/layouts/controllers/initialize.tsx
+++ src/layouts/controllers/initialize.tsx
@@ -3,15 +3,22 @@
 import { useEffect } from 'react'
 // import { useShallow } from 'zustand/shallow'
 import { useAppState } from '@/store'
+import { trpc } from '@trpc'
 import Console from './initialize.console'
 
 export const Initialize = () => {
   const _onAppInit = useAppState((st) => st.onAppInit)
+  const { data: searchDataIndex } = trpc.web.search.getSearchIndex.useQuery()
 
   useEffect(() => {
     console.log(Console)
-    _onAppInit()
   }, [])
+
+  useEffect(() => {
+    if (searchDataIndex && searchDataIndex.success) {
+      _onAppInit({ searchDataIndex: searchDataIndex.data })
+    }
+  }, [searchDataIndex])
 
   return null
 }

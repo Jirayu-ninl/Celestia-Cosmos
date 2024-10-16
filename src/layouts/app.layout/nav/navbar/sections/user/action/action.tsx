a7c444996ca4b3755411f8f30f107f01bf9459fd
Jirayu-ninl <Jirayu.ninl@gmail.com>
2024-10-16 22:26:06 +0700

    [fix] ui: app-layout/nav/user/action -> bg transparent

--- src/layouts/app.layout/nav/navbar/sections/user/action/action.tsx
+++ src/layouts/app.layout/nav/navbar/sections/user/action/action.tsx
@@ -10,7 +10,7 @@ export const UserAction: React.FC<UserActionProps> = ({ session }) => {
   return (
     <>
       <motion.div
-        className='absolute -bottom-2 left-14 z-90 rounded-md border border-foreground/10 bg-background/20 p-3 backdrop-blur-md'
+        className='absolute -bottom-2 left-14 z-90 rounded-md border border-foreground/10 p-3 backdrop-blur-md bg-background/90'
         initial={{ y: 20, opacity: 0, x: 0 }}
         animate={{ y: 0, opacity: 1, x: 0 }}
         exit={{ y: 20, opacity: 0, x: 0 }}

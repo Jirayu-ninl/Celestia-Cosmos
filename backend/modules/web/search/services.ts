93d92febac61468bf77978cf8073629b1a30f174
Jirayu-ninl <Jirayu.ninl@gmail.com>
2024-10-16 21:28:39 +0700

    [refactor] search: add null safety

--- backend/modules/web/search/services.ts
+++ backend/modules/web/search/services.ts
@@ -3,18 +3,27 @@ import type { Context } from '@backend/trpc/trpc.context'
 import { getErrorMessage } from '@nexel/nextjs/utils/server/error'
 import { trpcResponse } from '@nexel/nextjs/utils/server/trpc'
 import { getPosts, getProjects, staticUrls } from '@/utils/sitemap'
+import { m } from 'framer-motion'
 
 export const getSearchIndex = async ({ ctx }: { ctx: Context }) => {
   try {
     const posts = await getPosts()
     const projects = await getProjects()
 
+    if (!projects) {
+      return trpcResponse.fail('Fetch posts failed')
+    }
+
+    if (!posts) {
+      return trpcResponse.fail('Fetch projects failed')
+    }
+
     const searchIndex = [
       ...staticUrls.map(({ title, url: path }) => ({ title, path })),
       ...projects.map(({ title, url: path }: any) => ({ title, path })),
       ...posts.map(({ title, url: path }: any) => ({ title, path })),
     ]
-    return trpcResponse.success('Login complete', {
+    return trpcResponse.success('Get search index complete', {
       data: searchIndex,
       // metadata: { session: null },
     })

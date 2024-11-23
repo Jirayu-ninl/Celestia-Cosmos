a56b30c852fd8e8688818e31abf0ffcd33463699
Jirayu-ninl <Jirayu.ninl@gmail.com>
2024-11-23 23:29:59 +0700

    [dep] server: trpc -> upgrade version and refactor

--- backend/trpc/client/index.ts
+++ backend/trpc/client/index.ts
@@ -1,13 +1,4 @@
-import type { AppRouter } from '../../controllers'
+import type { Router } from '../../controllers'
 import { createTRPCReact } from '@trpc/react-query'
 
-export const trpc = createTRPCReact<AppRouter>({
-  unstable_overrides: {
-    useMutation: {
-      async onSuccess(opts) {
-        await opts.originalFn()
-        await opts.queryClient.invalidateQueries()
-      },
-    },
-  },
-})
+export const trpc = createTRPCReact<Router>({})

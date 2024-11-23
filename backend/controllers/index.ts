554fcfdff745251c74fe6624cb244467a066cdc5
Jirayu-ninl <Jirayu.ninl@gmail.com>
2024-11-23 23:28:55 +0700

    [refactor] server: trpc -> change variable

--- backend/controllers/index.ts
+++ backend/controllers/index.ts
@@ -3,12 +3,11 @@ import { exampleRouter } from './example'
 import { userRouter } from './user'
 import { webRouter } from './web'
 
-const router = createTRPCRouter({
+export const router = createTRPCRouter({
   example: exampleRouter,
   user: userRouter,
   web: webRouter,
 })
 
 export const AppController = { router }
-
-export type AppRouter = typeof router
+export type Router = typeof router
